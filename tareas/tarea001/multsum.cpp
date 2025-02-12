defab73b3bf7321363085404edba07e8a4f1f43d
AustinRodrigue <austin.rodriguez.0212@gmail.com>
2025-02-11 21:44:00 -0600

    Agregados los programas hello_world.cpp, multisum.cpp y el README.md

---
+++ tareas/tarea001/multsum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    
+  
+    int num1, num2;
+    std::cout << "Ingresar un numero: ";
+    std::cin >> num1;
+    std::cout << "Ingresar otro numero: ";
+    std::cin >> num2;
+    std::cout << "Suma: " << (num1 + num2) << std::endl;
+    std::cout << "Multiplicacion: " << (num1 * num2)<<std::endl;
+ 
+
+return 0;
+}
